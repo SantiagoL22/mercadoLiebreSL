e0f04e5732b2d4dc814fa04ef17c6270bb44b4b9
unknown <santiagolara425@ymail.com>
2023-01-04 05:50:47 -0300

    agregue el archivo app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000, () => {
+  console.log("Metéle mecha");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/login.html"));
+});
